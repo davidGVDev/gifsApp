581af0921f21cf846a59d30d934c82188bc7ec7c
David <david38ing@gmail.com>
2023-12-23 12:30:23 -0600

    first-commit app gifs

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,16 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrl: './search-box.component.css',
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
+}
